fd2fda1c5bc95b813fd9aa0439b6020321652e61
SHUBHAM2775 <shubhamu1332@gmail.com>
2025-06-12 23:29:48 +0530

    commit

--- Frontend/src/components/Landing.jsx
+++ Frontend/src/components/Landing.jsx
@@ -137,8 +137,14 @@ const Landing = () => {
                 Get your portfolio as a high-quality PDF ready for applications and sharing.
               </li>
             </ul>
-            <div className="w-full sm:w-155 h-40 sm:h-75 border-2 bg-gradient-to-br from-blue-600 to-purple-500 mt-4 sm:mt-1 border-gray-300 rounded-lg flex items-center justify-center bg-white shadow-md">
-              <p className="text-2xl">⁜</p>
+            <div className="w-full sm:w-155 h-40 sm:h-75 border-2 bg-gradient-to-br from-blue-600 to-purple-500 mt-4 sm:mt-1 border-gray-300 rounded-lg flex items-center justify-center shadow-md">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-sparkles h-20 w-20 text-white animate-pulse">
+              <path d="M9.937 15.5A2 2 0 0 0 8.5 14.063l-6.135-1.582a.5.5 0 0 1 0-.962L8.5 9.936A2 2 0 0 0 9.937 8.5l1.582-6.135a.5.5 0 0 1 .963 0L14.063 8.5A2 2 0 0 0 15.5 9.937l6.135 1.581a.5.5 0 0 1 0 .964L15.5 14.063a2 2 0 0 0-1.437 1.437l-1.582 6.135a.5.5 0 0 1-.963 0z"></path>
+              <path d="M20 3v4"></path>
+              <path d="M22 5h-4"></path>
+              <path d="M4 17v2"></path>
+              <path d="M5 18H3"></path>
+              </svg>
             </div>
           </div>
         </div>
