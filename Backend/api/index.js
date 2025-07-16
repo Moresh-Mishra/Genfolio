8545c488a445fec0bf701a82da6c8bb9acefa162
SHUBHAM2775 <shubhamu1332@gmail.com>
2025-07-16 19:02:36 +0530

    api2

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -12,8 +12,8 @@ app.use(express.json({ limit: '50mb' }));
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://genfolioupdates.vercel.app',
-    'https://genfolio-eight.vercel.app'
+    'https://genfolio-eight.vercel.app',
+    'https://genfolio-one.vercel.app'
   ],
   credentials: true
 }));
