b33f628a2589eca2b4938f8fdcc0291109894907
Moresh-Mishra <moreshmishra@gmail.com>
2025-08-14 22:50:48 +0530

    updates-again

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -9,6 +9,7 @@ const session = require('express-session');
 
 const app = express();
 app.use(express.json({ limit: '50mb' }));
+app.use(cors());
 app.use(cors({
   origin: [
     'http://localhost:5173',
